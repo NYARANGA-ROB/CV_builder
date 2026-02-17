03f34fb238c0d82bccd41e7d729cd737e6204658
Robert Nyaranga <101572972+NYARANGA-ROB@users.noreply.github.com>
2026-02-17 18:33:26 +0300

    Fix formatting issues in entrypoint.sh

--- website/scripts/entrypoint.sh
+++ website/scripts/entrypoint.sh
@@ -3,10 +3,10 @@
 set -e
 
 RUN_MANAGE_PY='poetry run python -m mytenderweb.manage'
-
 echo 'Collecting static files...'
 $RUN_MANAGE_PY collectstatic --no-input
 
 
 echo 'Starting Gunicorn server...'
 exec poetry run gunicorn mytenderweb.project.wsgi:application --bind 0.0.0.0:8000
+
