2299aded1c0a20c4e11f8845ffbf3a7db9263153
Robert Nyaranga <101572972+NYARANGA-ROB@users.noreply.github.com>
2026-03-02 01:06:16 +0300

    Update CustomDateInput.tsx

--- src/buttons/CustomDateInput.tsx
+++ src/buttons/CustomDateInput.tsx
@@ -19,7 +19,6 @@ const CustomDateInput = ({
       }
     }
   }, [value, defaultValue]);
-
   const formatDate = (date) => {
     const day = date.getDate().toString().padStart(2, "0");
     const month = (date.getMonth() + 1).toString().padStart(2, "0");
@@ -68,4 +67,5 @@ const CustomDateInput = ({
 
 
 export default CustomDateInput;
+
 
