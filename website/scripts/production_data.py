0a39d1b4f9eccd577259e49383843aed5248b320
Robert Nyaranga <101572972+NYARANGA-ROB@users.noreply.github.com>
2026-03-03 02:04:28 +0300

    Update production_data.py

--- website/scripts/production_data.py
+++ website/scripts/production_data.py
@@ -3,10 +3,10 @@ def generate_secret_key():
     secret_key = get_random_secret_key()
     print(f'SECRET_KEY: {secret_key}')
 
-
 if __name__ == '__main__':
     print()
 
     generate_secret_key()
+
 
 
