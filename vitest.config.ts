97f00acef99299adfcc2be11db8bbac1fdd3da2e
Robert Nyaranga <101572972+NYARANGA-ROB@users.noreply.github.com>
2026-03-03 18:50:23 +0300

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-
 export default defineConfig({
   plugins: [react()],
   test: {
@@ -15,4 +14,5 @@ export default defineConfig({
     // },
   }
 });
+
 
