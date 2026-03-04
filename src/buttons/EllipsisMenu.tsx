1b61edea98ac68797a56cf9afb2595f9747d7c1d
Robert Nyaranga <101572972+NYARANGA-ROB@users.noreply.github.com>
2026-03-04 11:13:12 +0300

    Update EllipsisMenu.tsx

--- src/buttons/EllipsisMenu.tsx
+++ src/buttons/EllipsisMenu.tsx
@@ -13,7 +13,6 @@ interface EllipsisMenuProps {
   // Add isMoving state
   isMoving?: boolean;
 }
-
 const EllipsisMenu: React.FC<EllipsisMenuProps> = ({
   filename,
   unique_id,
@@ -140,6 +139,7 @@ const EllipsisMenu: React.FC<EllipsisMenuProps> = ({
   );
 };
 export default EllipsisMenu;
+
 
 
 
