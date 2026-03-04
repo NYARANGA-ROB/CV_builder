822c4723f7862cf062d790423d90f15e39b6c4a0
Robert Nyaranga <101572972+NYARANGA-ROB@users.noreply.github.com>
2026-03-04 16:24:21 +0300

    Update manage.py

--- website/mytenderweb/manage.py
+++ website/mytenderweb/manage.py
@@ -3,6 +3,7 @@
 import os
 import sys
 def main():
+    
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mytenderweb.project.settings')
     try:
@@ -16,6 +17,7 @@ def main():
     execute_from_command_line(sys.argv)
 if __name__ == '__main__':
     main()
+
 
 
 
