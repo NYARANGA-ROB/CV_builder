6464ce89bc800a1768e27bf2432af503a756c818
Robert Nyaranga <101572972+NYARANGA-ROB@users.noreply.github.com>
2026-03-19 14:11:41 +0300

    Update BidExtractorWizard.tsx

--- src/wizards/BidExtractorWizard.tsx
+++ src/wizards/BidExtractorWizard.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Wizard from "react-onboarding";
 import CustomWizard from "./CustomWizard";
-
 const BidExtractorWizard = () => {
   const [isShow, setIsShow] = useState(false);
   const steps = [
