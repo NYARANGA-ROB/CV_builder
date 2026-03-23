7d9945c792ca7ab0ffef8b649fa97e201e9e44c4
Robert Nyaranga <101572972+NYARANGA-ROB@users.noreply.github.com>
2026-03-23 15:40:05 +0300

    Update EllipsisMenuDashboard.tsx

--- src/buttons/EllipsisMenuDashboard.tsx
+++ src/buttons/EllipsisMenuDashboard.tsx
@@ -9,7 +9,6 @@ const EllipsisMenuDashboard: React.FC<EllipsisMenuDashboardProps> = ({
   onClick
 }) => {
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-
   const handleClick = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget);
   };
