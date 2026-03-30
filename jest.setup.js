d8cb95319c3c842b84db066199fb13f61211af37
Robert Nyaranga <101572972+NYARANGA-ROB@users.noreply.github.com>
2026-03-30 15:45:13 +0300

    Update jest.setup.js

--- jest.setup.js
+++ jest.setup.js
@@ -1,7 +1,6 @@
 require("@testing-library/jest-dom");
 require("dotenv").config({ path: ".env.test" });
 // Existing window.matchMedia mock
-
 Object.defineProperty(window, "matchMedia", {
   writable: true,
   value: jest.fn().mockImplementation((query) => ({
