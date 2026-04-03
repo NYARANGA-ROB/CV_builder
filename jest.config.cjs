5873c3d4de8f0e4ecee93fc9857611a82f34647b
Robert Nyaranga <101572972+NYARANGA-ROB@users.noreply.github.com>
2026-04-03 10:53:45 +0300

    Update jest.config.cjs

--- jest.config.cjs
+++ jest.config.cjs
@@ -8,6 +8,7 @@ module.exports = {
     setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
     
     transform: {  
+        
         "^.+\\.[tj]sx?$": ["ts-jest", {
             useESM: true,
             tsconfig: {
