939c13c4f4f7807217698f80c22831a700d24d9d
Robert Nyaranga <101572972+NYARANGA-ROB@users.noreply.github.com>
2026-04-05 13:26:24 +0300

    Update SelectTenderLibraryFile.tsx

--- SelectTenderLibraryFile.tsx
+++ SelectTenderLibraryFile.tsx
@@ -11,6 +11,7 @@ import { Button, Card, Form, Spinner } from "react-bootstrap";
 import { faFileAlt } from "@fortawesome/free-solid-svg-icons";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import "../modals/SelectFolderModal.css";
 import { displayAlert } from "../helper/Alert";
 const SelectTenderLibraryFile = ({
