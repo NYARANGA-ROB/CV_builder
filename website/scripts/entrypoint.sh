6493f1e06da05f5314e4460c961597ab4d3ae471
Robert Nyaranga <101572972+NYARANGA-ROB@users.noreply.github.com>
2026-04-10 11:20:32 +0300

    Update entrypoint.sh

--- website/scripts/entrypoint.sh
+++ website/scripts/entrypoint.sh
@@ -6,6 +6,7 @@ echo 'Collecting static files...'
 $RUN_MANAGE_PY collectstatic --no-input
 
 echo 'Starting Gunicorn server...'
+
 exec poetry run gunicorn mytenderweb.project.wsgi:application --bind 0.0.0.0:8000
 
 
