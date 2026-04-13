0446890b13614f548ef7671373f4b202e1c60751
Robert Nyaranga <101572972+NYARANGA-ROB@users.noreply.github.com>
2026-04-13 17:00:04 +0300

    Update jest.config.cjs

--- jest.config.cjs
+++ jest.config.cjs
@@ -3,7 +3,6 @@ module.exports = {
     testEnvironment: "jest-environment-jsdom",
     moduleNameMapper: {
         "\\.(css|less|scss|sass)$": "identity-obj-proxy",
-        
         "\\.(jpg|jpeg|png|gif|webp|svg)$": "<rootDir>/__mocks__/fileMock.js"
         
     },
