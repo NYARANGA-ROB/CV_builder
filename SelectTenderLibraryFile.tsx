63ecf2835570badcf8f39b052b46c58dc0bd8641
Robert Nyaranga <101572972+NYARANGA-ROB@users.noreply.github.com>
2026-04-13 06:17:58 +0300

    Update SelectTenderLibraryFile.tsx

--- SelectTenderLibraryFile.tsx
+++ SelectTenderLibraryFile.tsx
@@ -16,6 +16,7 @@ const SelectTenderLibraryFile = ({
   const getAuth = useAuthUser();
   
   const auth = getAuth();
+  
   const tokenRef = useRef(auth?.token || "default");
   const [documents, setDocuments] = useState([]);
   const [documentListVersion, setDocumentListVersion] = useState(0);
