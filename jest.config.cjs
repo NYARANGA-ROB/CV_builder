270f2992c38d59e86cd98ada70c45f7e14bbdba8
Robert Nyaranga <101572972+NYARANGA-ROB@users.noreply.github.com>
2026-04-25 20:19:38 +0300

    Update jest.config.cjs

--- jest.config.cjs
+++ jest.config.cjs
@@ -14,7 +14,6 @@ module.exports = {
             }
         }]
     },
-    
     transformIgnorePatterns: ["node_modules/(?!(react-auth-kit|axios)/)"],
     moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
     extensionsToTreatAsEsm: [".ts", ".tsx"],
