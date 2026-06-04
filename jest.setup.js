53a1d48decc1c75f7876770ea3165a668b0b60b5
Robert Nyaranga <101572972+NYARANGA-ROB@users.noreply.github.com>
2026-06-04 09:37:29 +0300

    Refactor jest.setup.js to remove redundant code

--- jest.setup.js
+++ jest.setup.js
@@ -9,6 +9,7 @@ Object.defineProperty(window, "matchMedia", {
   writable: true,
   
   value: jest.fn().mockImplementation((query) => ({
+    
     matches: false,
     media: query,
     onchange: null,
