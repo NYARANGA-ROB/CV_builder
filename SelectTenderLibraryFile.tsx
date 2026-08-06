a9d80117ed717eb813183aaddf0bf13497c212b5
Robert Nyaranga <101572972+NYARANGA-ROB@users.noreply.github.com>
2026-08-06 11:35:32 +0300

    Update SelectTenderLibraryFile.tsx

--- SelectTenderLibraryFile.tsx
+++ SelectTenderLibraryFile.tsx
@@ -10,7 +10,6 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "../modals/SelectFolderModal.css";
 
 import { displayAlert } from "../helper/Alert";
-
 const SelectTenderLibraryFile = ({
   bid_id,
   onFileSelect,
