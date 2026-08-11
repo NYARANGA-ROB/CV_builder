f5e8e25464339122c32d5b52c824a3fa3d9ec8c1
Robert Nyaranga <101572972+NYARANGA-ROB@users.noreply.github.com>
2026-08-11 16:53:10 +0300

    Update jest.config.cjs

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,5 +1,6 @@
 module.exports = {
     preset: "ts-jest/presets/js-with-ts-esm",
+    
     testEnvironment: "jest-environment-jsdom",
     moduleNameMapper: {
         "\\.(css|less|scss|sass)$": "identity-obj-proxy",
