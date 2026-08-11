43e8710f69bed4f27436b906c3609664da7dc446
Robert Nyaranga <101572972+NYARANGA-ROB@users.noreply.github.com>
2026-08-11 16:54:47 +0300

    Refactor jest.setup.js for better readability

--- jest.setup.js
+++ jest.setup.js
@@ -7,7 +7,6 @@ Object.defineProperty(window, "matchMedia", {
   writable: true,
   value: jest.fn().mockImplementation((query) => ({
     matches: false,
-    
     media: query,
     
     onchange: null,
